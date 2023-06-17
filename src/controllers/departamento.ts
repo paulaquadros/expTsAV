ce5553ff844d8dd87c43c5ddd89e192af77dd093
Paula Quadros <paula.mendonca@icomp.ufam.edu.br>
2023-06-17 09:51:50 -0400

    creating departamento page and form...

--- src/controllers/departamento.ts
+++ src/controllers/departamento.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from 'express';
+import { Departamentos } from '../models/Departamentos';
 
-const index = async (req: Request, res: Response) => {};
-const create = async (req: Request, res: Response) => {};
+const index = async (req: Request, res: Response) => {
+  const departamentos = await Departamentos.findAll();
+  res.render('departamento/index', { departamentos });
+};
+const create = async (req: Request, res: Response) => {
+  if (req.route.methods.get) {
+    res.render('departamento/create');
+  }
+};
 const read = async (req: Request, res: Response) => {};
 const update = async (req: Request, res: Response) => {};
 const remove = async (req: Request, res: Response) => {};
